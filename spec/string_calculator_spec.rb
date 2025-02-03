ec5f5ed21b388c6b563c6d7d7b009e894eb21fcc
pramakrishna <pramakrishna@plansource.com>
2025-02-03 16:21:01 +0530

    test case for string of comma-separated numbers

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,19 @@
+require 'rspec'
+require_relative '../string_calculator'
+
+RSpec.describe StringCalculator do
+  let(:calculator) { StringCalculator.new }
+
+  it "returns 0 for an empty string" do
+    expect(calculator.add("")).to eq(0)
+  end
+
+  it "returns the number itself for a single number" do
+    expect(calculator.add("1")).to eq(1)
+  end
+
+  it "returns the sum of two numbers" do
+    expect(calculator.add("1,5")).to eq(6)
+  end
+end
+
