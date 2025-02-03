70b3ca9f3881edf954126305a494a14c02ee678a
pramakrishna <pramakrishna@plansource.com>
2025-02-03 19:23:24 +0530

    added test case for handling multiple negative numbers case

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -27,5 +27,12 @@ RSpec.describe StringCalculator do
   it "supports different delimiters" do
     expect(calculator.add("//;\n1;2")).to eq(3)
   end
-end
 
+  it "raises an exception for negative numbers" do
+    expect { calculator.add("1,-2,3") }.to raise_error("negative numbers not allowed: -2")
+  end
+
+  it "raises an exception for multiple negative numbers" do
+    expect { calculator.add("1,-2,-3") }.to raise_error("negative numbers not allowed: -2, -3")
+  end
+end
--- string_calculator.rb
+++ string_calculator.rb
@@ -9,6 +9,10 @@ class StringCalculator
       numbers = parts[1]
     end
 
-    numbers.split(/#{delimiter}/).map(&:to_i).sum
+    nums = numbers.split(/#{delimiter}/).map(&:to_i)
+    negatives = nums.select { |n| n < 0 }
+    raise "negative numbers not allowed: #{negatives.join(', ')}" unless negatives.empty?
+
+    nums.sum
   end
 end
