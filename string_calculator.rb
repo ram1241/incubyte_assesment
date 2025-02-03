9eaf014528649b56e9a5278fa98a574ce4d813a2
pramakrishna <pramakrishna@plansource.com>
2025-02-03 18:57:02 +0530

    added test case for supporting different delimiters

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,6 +2,13 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(/,|\n/).map(&:to_i).sum
+    delimiter = ",|\n"
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      delimiter = Regexp.escape(parts[0][2])
+      numbers = parts[1]
+    end
+
+    numbers.split(/#{delimiter}/).map(&:to_i).sum
   end
 end
